34f3931560b749c287ff8686f329e0bc2cdaebbb
jonah katz <jak1192@gmail.com>
2016-01-07 12:45:32 -0500

    Oauth callback.

--- app/controllers/slack_interface_controller.rb
+++ app/controllers/slack_interface_controller.rb
@@ -5,7 +5,11 @@ class SlackInterfaceController < ApplicationController
 		render json:{"text" => "WOW" << params.inspect}
 	end
 
-	def oauth 
+	def authorize_slack 
 		redirect_to "https://slack.com/oauth/authorize?client_id=17965490070.17965037873&scope=chat:write:bot"
+	end
+
+	def oauth
+		redirect_to "https://slack.com/api/oauth.access?code=#{params[:code]}&client_id=17965490070.17965037873&client_secret=b8ac0e145d22b4bd13041c91873f47eb"
 	end
 end
