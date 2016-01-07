e5edb15719972118c08305c3cf955a295bf639fb
jonah katz <jak1192@gmail.com>
2016-01-07 14:24:13 -0500

    typo

--- app/services/slack_command_parser_service.rb
+++ app/services/slack_command_parser_service.rb
@@ -40,7 +40,7 @@ class SlackCommandParser
   end
 
   def projects
-  	reponse = ''
+  	response = ''
 	toggle_request.my_projects.each do |p|
   		response.push p['name'] << " - "<< p['id'] << "\n"
   	end
