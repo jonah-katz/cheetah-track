d1d8c2f655d9fdf3bad43541fd09538703b17d7e
jonah katz <jak1192@gmail.com>
2016-01-07 14:12:18 -0500

    Add more commands.

--- app/services/slack_command_parser_service.rb
+++ app/services/slack_command_parser_service.rb
@@ -1,5 +1,5 @@
 class SlackCommandParser
-	attr_accessor :slack_user_id,:text, :toggl_account
+	attr_accessor :slack_user_id,:text, :toggl_account,:api_instance
   def command(params)
   	@text = params[:text]
   	@slack_user_id = params[:user_id]
@@ -16,6 +16,10 @@ class SlackCommandParser
   		parse_setup_command
   	when 'projects'
   		projects
+  	when 'status'
+  		status
+  	when 'start'
+  		start
   	else
   		"I don't know that command. :("
   	end
@@ -36,12 +40,69 @@ class SlackCommandParser
   end
 
   def projects
-  	toggle_request.my_projects.inspect
+  	projects = toggle_request.my_projects.inspect
+  	response = ''
+  	projects.each do |p|
+  		response.push p.id << " - " << p.name << "\n"
+  	end
+  	response
+  end
+
+  # /slackl status
+  def status
+  	te = toggle_request.get_current_time_entry
+  	if !te
+  		return "Timer isn't currently active."
+  	end
+
+  	return_t = humanize Time.new.to_i + te['duration']
+  	project = false
+  	if te.pid
+  		project = getProjectById(te['pid'])
+  		return_t << ' - ' << project['name']
+  	end
+  	if te.description
+  		return_t << ' - ' << te['description']
+  	end
+
+  	return return_t;
+
+  end
+
+  # /slackl start <project id> <optional description>
+  def start
+  	project_id = @text.split[1]
+  	project 
+  	if !project_id
+  		return "Couldn't find a project with an id `" << @project_id << "`" 
+  	end
+
+  	description = @text.split[1..-2].join(' ')
+  	if toggle_request.start_time_entry({'pid' => project_id, 'description' => description})
+  		return "Timers going!"
+  	end
+
+  	return "Something went wrong :/ Try again?"
+  	
+
+  end
+
+  def getProjectById
+	toggle_request.my_projects.each do |p|
+		if p['id'] == te.pid
+			return p
+		end
+	end
+	return false
   end
 
 
   def toggle_request
-  	TogglV8::API.new(@toggl_account.api_token)
+  	if @api_instance
+  		return @api_instance
+  	end
+  	@api_instance = TogglV8::API.new(@toggl_account.api_token)
+  	return @api_instance
   end
 
   def get_toggle_account
@@ -52,5 +113,14 @@ class SlackCommandParser
   		@toggl_account = ta.first
   	end
   end
+
+	def humanize secs
+	  [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
+	    if secs > 0
+	      secs, n = secs.divmod(count)
+	      "#{n.to_i} #{name}"
+	    end
+	  }.compact.reverse.join(' ')
+	end
 
 end
