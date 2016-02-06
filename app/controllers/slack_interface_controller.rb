7d2131bd79d70b050de225242814cd2721e751bf
jonah katz <jak1192@gmail.com>
2016-02-06 00:27:16 -0500

    Removed no longer in user code from slack interface controller.

--- app/controllers/slack_interface_controller.rb
+++ app/controllers/slack_interface_controller.rb
@@ -4,7 +4,4 @@ class SlackInterfaceController < ApplicationController
 		render json:{"text" => SlackCommandParser.new.command(params)}
 	end
 
-	def oauth
-		redirect_to "https://slack.com/api/oauth.access?code=#{params[:code]}&client_id=#{ENV['SLACK_CLIENT_ID']}&client_secret=#{ENV['SLACK_CLIENT_SECRET']}"
-	end
 end
