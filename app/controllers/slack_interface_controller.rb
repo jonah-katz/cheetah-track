6b81d5d55b8749e5925d87ba8cbc021f64daa27e
jonah katz <jak1192@gmail.com>
2016-02-05 23:55:36 -0500

    Added to slack success message.

--- app/controllers/slack_interface_controller.rb
+++ app/controllers/slack_interface_controller.rb
@@ -4,10 +4,6 @@ class SlackInterfaceController < ApplicationController
 		render json:{"text" => SlackCommandParser.new.command(params)}
 	end
 
-	def authorize_slack 
-		redirect_to "https://slack.com/oauth/authorize?client_id=#{ENV['SLACK_CLIENT_ID']}&scope=chat:write:bot"
-	end
-
 	def oauth
 		redirect_to "https://slack.com/api/oauth.access?code=#{params[:code]}&client_id=#{ENV['SLACK_CLIENT_ID']}&client_secret=#{ENV['SLACK_CLIENT_SECRET']}"
 	end
