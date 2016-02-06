8bc217935ccb7f3a74cf1c293d1a1ea2623cfe5d
jonah katz <jak1192@gmail.com>
2016-02-05 23:43:38 -0500

    Visitors page design.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,10 +1,5 @@
 class VisitorsController < ApplicationController
 	def index
-		if user_signed_in?
-			@toggls = TogglAccount.where(user_id: current_user.id)
-			render 'authenticated'
-		else
-			render 'index'
-		end
+		render 'index'
 	end
 end
