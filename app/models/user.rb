b676cbdd99e062541051ca72d44dcd88705ab16e
jonah katz <jak1192@gmail.com>
2016-02-05 22:19:35 -0500

    Remove unused code and files.

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
-  post 'toggl_accounts', to: 'toggl_accounts#create'
-  delete 'toggl_accounts/:toggl_account_id', to: 'toggl_accounts#destroy'
-
 
   # slack
   post 'slackinterface', to: 'slack_interface#index'
