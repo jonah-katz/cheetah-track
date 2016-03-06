7d96b0753bab6551c478079791bdc654d4f193e6
jonah katz <jak1192@gmail.com>
2016-03-06 13:48:47 -0500

    Oath.

--- app/controllers/slack_interface_controller.rb
+++ app/controllers/slack_interface_controller.rb
@@ -1,7 +1,18 @@
+require 'net/http'
+
+
 class SlackInterfaceController < ApplicationController
 
 	def index
 		render json:{"text" => SlackCommandParser.new.command(params)}
 	end
 
+	def oauth
+  		url =  "https://slack.com/api/oauth.access?code=#{params[:code]}&client_id=#{ENV['SLACK_CLIENT_ID']}&client_secret=#{ENV['SLACK_CLIENT_SECRET']}"
+		url = URI.parse(url)
+		req = Net::HTTP::Get.new(url.to_s)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+			redirect_to '/?added_to_slack=true'
+		}
+  	end
 end
